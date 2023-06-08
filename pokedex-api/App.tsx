c4a22c164e38d42e3b792f2cd11c523275cda427
GeorgesB <georgesballister.profissional@gmail.com>
2023-06-07 22:29:23 -0300

    Api Funcionando e Sendo Puxada Para o Console

--- pokedex-api/App.tsx
+++ pokedex-api/App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
-import { Welcome } from './src/Pages/Welcome';
+///import { Welcome } from './src/Pages/Welcome';
 import { Home } from './src/Pages/Home';
 
 export default function App() {
   return (
    <ThemeProvider theme={theme}>
-    <Welcome />
+    
 
     <Home />
    </ThemeProvider>
--- pokedex-api/src/Pages/Home/index.tsx
+++ pokedex-api/src/Pages/Home/index.tsx
@@ -1,13 +1,57 @@
-import React from "react";
-
-import { Button } from "../../components/Button";
-
+import React, { useEffect } from "react";
+import api from '../../service/api'
+import * as S from './styles'
 
 
 
 // Tela Inicial com o GIF do Squirtle
 export function Home() {
+
+
+    useEffect(() => {
+        async function getAllPokemons() {
+            const response = await api.get('/pokemon')
+            const { results } = response.data;
+
+
+            const payloadPokemons = await Promise.all(
+
+                results.map(async pokemon => {
+                    const { id, types } = await getMoreInfo(pokemon.url)
+
+                    return {
+                        name: pokemon.name,
+                        id,
+                        types
+                    }
+
+                
+
+                })
+
+            )
+
+
+            console.log(payloadPokemons)
+
+        }
+
+
+        getAllPokemons()
+    }, [])
+
+
+    async function getMoreInfo(url: string) {
+        const response = await api.get(url)
+        const { id, types } = response.data;
+
+        return {
+            id, types
+        }
+    }
+
+
     return <S.Container>
-        
+
     </S.Container>
 }
